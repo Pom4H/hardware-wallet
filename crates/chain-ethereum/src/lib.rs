5df36c5174698ac727c7d0a27a480cbe78d7f519
Roman Popov <atfpom4h@gmail.com>
2026-08-28 07:58:33 +0300

    Define Ethereum module boundary

---
+++ crates/chain-ethereum/src/lib.rs
@@ -0,0 +1,38 @@
+#![no_std]
+
+use hardware_wallet_chain_api::{ChainId, ChainModule};
+
+pub struct Ethereum;
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct Request;
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct Review;
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct SigningRequest;
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum Error {
+    NotImplemented,
+}
+
+impl ChainModule for Ethereum {
+    type Request = Request;
+    type Review = Review;
+    type SigningRequest = SigningRequest;
+    type Error = Error;
+
+    const ID: ChainId = ChainId("ethereum");
+
+    fn prepare_review(_request: &Self::Request) -> Result<Self::Review, Self::Error> {
+        Err(Error::NotImplemented)
+    }
+
+    fn prepare_signing(
+        _review: &Self::Review,
+    ) -> Result<Self::SigningRequest, Self::Error> {
+        Err(Error::NotImplemented)
+    }
+}
